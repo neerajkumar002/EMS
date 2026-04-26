fea843a62084b00e7ac6f232017902ff6ab635b5
Neeaj Kumar <neerajkumar08697@gmail.com>
2026-04-26 13:42:52 +0530

    add employee class

---
+++ admin/employee/Employee.php
@@ -0,0 +1,23 @@
+<?php
+
+class Employee
+{
+
+
+    //methods
+    //add employee
+    public function addEmployee($empID,$name, $email, $phone, $address, $position, $department, $salary, $joinDate,  $status) {
+      
+
+    }
+
+    //delete employe
+    public function deleteEmployee() {}
+
+    //edit employee 
+    public function editEmployee() {}
+
+    //
+
+
+}
